6338eb854d26ed979903ba86849a3ab7fb271a71
mansi <kachhiyamanshi15042810@gmail.com>
2022-09-08 11:16:09 +0530

    add root reducer file

--- src/Redux/Reducer/rootReducer.js
+++ src/Redux/Reducer/rootReducer.js
@@ -2,6 +2,5 @@ import {combineReducers} from "redux"
 import {cartData} from "./reducer";
 import {ProductData} from "./ProductReducer"
 export default combineReducers({
-cartData,
-    ProductData
+cartData, ProductData
 })
